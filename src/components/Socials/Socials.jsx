1bf4ce9e1a25f9b3e710e73243ef6bfaf33f108c
Sabyrka <sabyrkazan@gmail.com>
2026-03-14 21:20:41 +0500

    chore: add Socials component with icon support and dynamic social items

---
+++ src/components/Socials/Socials.jsx
@@ -0,0 +1,19 @@
+import { Icon } from '@/components/Icon'
+import { socialItems } from './socials.data'
+
+export default () => {
+  return (
+    <div className="soc1als header__soc1als">
+      <ul className="soc1als__list">
+        {socialItems.map(({ label, icon }) => (
+          <li className="soc1als__item" key={label}>
+            <a className="soc1als__link" href="/" title={label}>
+              <span className="visually-hidden">{label}</span>
+              <Icon name={icon} hasFill />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
