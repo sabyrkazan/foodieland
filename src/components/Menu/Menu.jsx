458df55c66ea9c2e4f04ad8accd15c7d1b3a4b9f
Sabyrka <sabyrkazan@gmail.com>
2026-03-14 23:08:12 +0500

    feat: highlight active menu link based on current URL

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,13 +1,16 @@
 import './Menu.scss'
 import clsx from 'clsx'
 
-export default ({ className, items }) => {
+export default ({ className, items, url }) => {
   return (
     <nav className={clsx('menu', className)}>
       <ul className="menu-list">
         {items.map(({ label, href }) => (
           <li className="menu-item" key={label}>
-            <a className="menu-link" href={href}>
+            <a
+              className={clsx('menu-link', href === url && 'is-active')}
+              href={href}
+            >
               {label}
             </a>
           </li>
