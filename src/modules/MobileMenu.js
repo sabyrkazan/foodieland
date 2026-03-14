0f9c105d88178207317e7831c43a8c9ec443df88
Sabyrka <sabyrkazan@gmail.com>
2026-03-15 03:11:20 +0500

    feat: implement MobileMenu class with toggle functionality and event bindings

--- src/modules/MobileMenu.js
+++ src/modules/MobileMenu.js
@@ -1,1 +1,31 @@
-export const MobileMenu = () => {}
+export class MobileMenu {
+  selectors = {
+    root: '[data-js-mobile-menu]',
+    overlay: '[data-js-mobile-menu-overlay]',
+    burgerButton: '[data-js-mobile-menu-burger-button]',
+  }
+
+  stateClasses = {
+    isActive: 'is-active',
+    isLock: 'is-lock',
+  }
+
+  constructor() {
+    this.rootElement = document.querySelector(this.selectors.root)
+    this.overlayElement = this.rootElement.querySelector(this.selectors.overlay)
+    this.burgerButtonElement = this.rootElement.querySelector(
+      this.selectors.burgerButton
+    )
+    this.bindEvents()
+  }
+
+  onBurgerButtonClick = () => {
+    this.overlayElement.classList.toggle(this.stateClasses.isActive)
+    this.burgerButtonElement.classList.toggle(this.stateClasses.isActive)
+    document.documentElement.classList.toggle(this.stateClasses.isLock)
+  }
+
+  bindEvents() {
+    this.burgerButtonElement.addEventListener('click', this.onBurgerButtonClick)
+  }
+}
