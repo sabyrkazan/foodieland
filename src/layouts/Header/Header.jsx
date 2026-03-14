6202b27d8af3d368112ed3c04841cbd2b5629456
Sabyrka <sabyrkazan@gmail.com>
2026-03-14 23:06:42 +0500

    chore: refactor Header to use Menu component and remove redundant styles in Header.scss

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,6 +1,7 @@
 import './Header.scss'
 import { Logo } from '@/components/Logo'
 import { Socials } from '@/components/Socials'
+import { Menu } from '@/components/Menu'
 import { menuItems } from './header.data'
 
 export default ({ url }) => {
@@ -8,18 +9,8 @@ export default ({ url }) => {
     <header className="header" data-js-overlay-menu="">
       <div className="header__inner container">
         <Logo className="header__logo" loading="eager" />
-        <nav className="header__menu">
-          <ul className="header__menu-list">
-            {menuItems.map(({ label, href }) => (
-              <li className="header__menu-item" key={label}>
-                <a className="header__menu-link" href={href}>
-                  {label}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <Socials />
+        <Menu className="header__menu" items={menuItems} />
+        <Socials className="header__socials" />
       </div>
     </header>
   )
