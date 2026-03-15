d2a921551d8654486fea3ba802cdc2b316847b70
Sabyrka <sabyrkazan@gmail.com>
2026-03-15 23:21:50 +0500

    feat: add responsive column support to Grid component with dynamic styles

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,5 +1,14 @@
 import './Grid.scss'
+import clsx from 'clsx'
 
-export default () => {
-  return <div>Grid</div>
+export default ({ children, columns }) => {
+  return (
+    <ul className={clsx('grid', `grid--${columns}`, className)}>
+      {children.map((child, index) => (
+        <li className="grid__item" key={index}>
+          {child}
+        </li>
+      ))}
+    </ul>
+  )
 }
