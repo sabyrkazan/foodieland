cacd781620221508910aaca7012c7a876c9e8a98
Sabyrka <sabyrkazan@gmail.com>
2026-03-15 16:38:00 +0500

    feat: improve AuthorCard layout with new body structure and updated SCSS styling

--- src/components/AuthorCard/AuthorCard.jsx
+++ src/components/AuthorCard/AuthorCard.jsx
@@ -6,10 +6,12 @@ export default ({ className, imgSrc, name, date }) => {
   return (
     <div className={clsx('author-card', className)}>
       <Image className="author-card__image" src={imgSrc} />
-      <h3 className="author-card__name">{name}</h3>
-      <time className="author-card__date" dateTime={date.dateTime}>
-        {date.label}
-      </time>
+      <div className="author-card__body">
+        <h3 className="author-card__name">{name}</h3>
+        <time className="author-card__date" dateTime={date.dateTime}>
+          {date.label}
+        </time>
+      </div>
     </div>
   )
 }
