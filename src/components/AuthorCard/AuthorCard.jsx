22d971378a8a3bfa94b53303510ff77605956ffc
Sabyrka <sabyrkazan@gmail.com>
2026-03-15 15:58:51 +0500

    feat: add AuthorCard component with SCSS styling and default export

---
+++ src/components/AuthorCard/AuthorCard.jsx
@@ -0,0 +1,15 @@
+import './AuthorCard.scss'
+import clsx from 'clsx'
+import { Image } from 'minista'
+
+export default ({ className, imgSrc, name, date }) => {
+  return (
+    <div className={clsx('author-card', className)}>
+      <Image className="author-card__image" src={imgSrc} />
+      <h3 className="author-card__name">{name}</h3>
+      <time className="author-card__date" dateTime={date.dateTime}>
+        {date.label}
+      </time>
+    </div>
+  )
+}
