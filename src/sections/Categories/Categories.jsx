332e8ffd93a853c263987ddfb94f13ea6998cf6e
Sabyrka <sabyrkazan@gmail.com>
2026-03-15 23:10:20 +0500

    feat: update Categories section to use Section component with title and link

--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -1,5 +1,12 @@
 import './Categories.scss'
+import { Section } from '@/layouts/Section'
 
 export default () => {
-  return <div>Categories</div>
+  return (
+    <Section
+      title="Categories"
+      titleId="categories"
+      linkLabel="View All Categories"
+    ></Section>
+  )
 }
