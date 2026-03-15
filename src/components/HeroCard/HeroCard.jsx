8be44d73bba4692c89864cc64a0341458f1690c2
Sabyrka <sabyrkazan@gmail.com>
2026-03-15 16:39:32 +0500

    feat: restructure HeroCard body layout, update header and footer styles

--- src/components/HeroCard/HeroCard.jsx
+++ src/components/HeroCard/HeroCard.jsx
@@ -18,20 +18,22 @@ export default ({
   return (
     <article className={clsx('hero-card', className)}>
       <div className="hero-card__body">
-        <div className="hero-card__category">
-          <Image src={category.imgSrc} />
-          <span>{category.label}</span>
+        <div className="hero-card__header">
+          <div className="hero-card__category">
+            <Image src={category.imgSrc} />
+            <span>{category.label}</span>
+          </div>
+          <h2 className="hero-card__title h1">{title}</h2>
+          <div className="hero-card__description">
+            <p>{description}</p>
+          </div>
+          <Tags className="hero-card__tags" tags={tags} />
         </div>
-        <h2 className="hero-card__title h1">{title}</h2>
-        <div className="hero-card__description">
-          <p>{description}</p>
-        </div>
-        <Tags className="hero-card__tags" tags={tags} />
         <div className="hero-card__footer">
           <AuthorCard className="hero-card__author" {...author} />
           <Button className="hero-card__button">
             <span>View Recipes</span>
-            <Icon name='play' hasFill />
+            <Icon name="play" hasFill />
           </Button>
         </div>
       </div>
