e99ee53e83638b1125a6be9f5ed65226bff03433
Sabyrka <sabyrkazan@gmail.com>
2026-03-15 14:55:55 +0500

    feat: add slider functionality to Hero section with Swiper.js integration

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -3,7 +3,29 @@ import './Hero.scss'
 export default () => {
   return (
     <section className="hero" aria-labelledby="hero-title">
-      <div className="hero__inner container">Hero section content</div>
+      <h1 className="visually-hidden" id="hero-title">
+        Spicy delicious chicken wings
+      </h1>
+      <div
+        className="hero__slider swiper"
+        data-js-slider={JSON.stringify({
+          spaceBetween: 40,
+          slidesPerView: 'auto',
+          centeredSlides: true,
+          initialSlide: 1,
+          effect: 'coverflow',
+        })}
+      >
+        <ul className="hero__slider-list swiper-wrapper">
+          <li className="hero__slider-item swiper-slide">
+            <article className="hero__slider-card hero-card">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+              aperiam beatae consectetur distinctio, fugit harum quasi ratione
+              recusandae sit voluptates.
+            </article>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
