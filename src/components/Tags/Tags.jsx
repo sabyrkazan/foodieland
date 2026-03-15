f3c009730c813e84ba4514798a4493905043b23e
Sabyrka <sabyrkazan@gmail.com>
2026-03-15 16:33:20 +0500

    feat: enhance Tags component with icon and label rendering, improve styling with SCSS updates

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,5 +1,18 @@
 import './Tags.scss'
+import clsx from 'clsx'
+import { Icon } from '@/components/Icon'
 
-export default () => {
-  return <div>Tags</div>
+export default ({ className, tags }) => {
+  return (
+    <div className={clsx('tags', className)}>
+      <ul className="tags__list">
+        {tags.map(({ icon, label }) => (
+          <li className="tags__item" key={label}>
+            <Icon name={icon} hasFill />
+            <span className="tags__label">{label}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
