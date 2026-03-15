f7d3a550955cd03f01f2cc15c25f6579615d457c
Sabyrka <sabyrkazan@gmail.com>
2026-03-15 23:56:07 +0500

    feat: enhance CategoryCard with dynamic styles, image handling, and props for improved flexibility

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,5 +1,21 @@
 import './CategoryCard.scss'
+import { Image } from 'minista'
 
-export default () => {
-  return <div>CategoryCard</div>
+export default ({ color, imgSrc, label }) => {
+  return (
+    <article className="category-card">
+      <div
+        className="category-card__body"
+        style={{ '--categoryCardColor': color }}
+      >
+        <div
+          className="category-card__image-wrapper"
+          style={{ '--catergodyCardImage': `url('${imgSrc}')` }}
+        >
+          <Image className="category-card__image" src={imgSrc} />
+        </div>
+        <h3 className="category-card__title">{label}</h3>
+      </div>
+    </article>
+  )
 }
