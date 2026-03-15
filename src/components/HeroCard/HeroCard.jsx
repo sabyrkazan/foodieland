2f2c296bd39d1d7d68344637d99f0e159f9b3d44
Sabyrka <sabyrkazan@gmail.com>
2026-03-15 15:58:58 +0500

    feat: enhance HeroCard component with props, detailed rendering, and support for category, tags, and author

--- src/components/HeroCard/HeroCard.jsx
+++ src/components/HeroCard/HeroCard.jsx
@@ -1,5 +1,50 @@
 import './HeroCard.scss'
+import clsx from 'clsx'
+import { Icon } from '@/components/Icon'
+import { Image } from 'minista'
+import { AuthorCard } from '@/components/AuthorCard'
+import Button from '@/components/Button'
+import buttonPlayIcon from '@/assets/icons/play.svg'
 
-export default () => {
-  return <div>HeroCard</div>
+export default ({
+  className,
+  category,
+  title,
+  description,
+  tags,
+  author,
+  imgSrc,
+}) => {
+  return (
+    <article className={clsx('hero-card', className)}>
+      <div className="hero-card__body">
+        <div className="hero-card__category">
+          <Image src={category.imgSrc} />
+          <span>{category.label}</span>
+        </div>
+        <h2 className="hero-card__title h1">{title}</h2>
+        <div className="hero-card__description">
+          <p>{description}</p>
+        </div>
+        <div className="hero-card__tags">
+          <ul className="hero-card__tags-list">
+            {tags.map(({ icon, label }) => (
+              <li className="hero-card__tags-item" key={label}>
+                <Icon name={icon} hasFill />
+                <span>{label}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="hero-card__footer">
+          <AuthorCard className="hero-card__author" {...author} />
+          <Button className="hero-card__button">
+            <span>View Recipes</span>
+            <Icon name={buttonPlayIcon} hasFill />
+          </Button>
+        </div>
+      </div>
+      <Image className="hero-card__image" src={imgSrc} />
+    </article>
+  )
 }
