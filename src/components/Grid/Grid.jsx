b1f5fe77daee7f68ef971f3aa4a755ed3d6d3e39
Sabyrka <sabyrkazan@gmail.com>
2026-03-15 23:56:12 +0500

    feat: add responsive modifiers to Grid component and support for custom class names

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,10 +1,11 @@
 import './Grid.scss'
 import clsx from 'clsx'
+import React from 'react'
 
-export default ({ children, columns }) => {
+export default ({ className, children, columns = 1 }) => {
   return (
     <ul className={clsx('grid', `grid--${columns}`, className)}>
-      {children.map((child, index) => (
+      {React.Children.map(children, (child, index) => (
         <li className="grid__item" key={index}>
           {child}
         </li>
