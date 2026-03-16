5db3291fefb84f296f265d12cf9b45a241c9f4c1
Sabyrka <sabyrkazan@gmail.com>
2026-03-16 16:12:41 +0500

    feat: enhance Recipes section with description styling and Section integration

--- src/sections/Recipes/Recipes.jsx
+++ src/sections/Recipes/Recipes.jsx
@@ -1,5 +1,14 @@
 import './Recipes.scss'
+import { Section } from '@/layouts/Section'
 
 export default () => {
-  return <div>Recipes</div>
+  return (
+    <Section
+      headerMode="vertical"
+      title="Simple and tasty recipes"
+      titleId="recipes"
+      description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim"
+      descriptionClassName="recipes__description"
+    ></Section>
+  )
 }
