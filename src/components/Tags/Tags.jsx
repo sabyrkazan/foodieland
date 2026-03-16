30f29481f0d0e112263f8c5d61745c04657efb08
Sabyrka <sabyrkazan@gmail.com>
2026-03-16 20:04:53 +0500

    refactor: rename 'tags' prop to 'items' in Tags component for clarity and consistency

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -2,11 +2,11 @@ import './Tags.scss'
 import clsx from 'clsx'
 import { Icon } from '@/components/Icon'
 
-export default ({ className, tags }) => {
+export default ({ className, items }) => {
   return (
     <div className={clsx('tags', className)}>
       <ul className="tags__list">
-        {tags.map(({ icon, label }) => (
+        {items.map(({ icon, label }) => (
           <li className="tags__item" key={label}>
             <Icon name={icon} hasFill />
             <span className="tags__label">{label}</span>
