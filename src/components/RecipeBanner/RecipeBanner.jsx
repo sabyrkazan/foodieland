e5322782fc4ed63622346bcc30c94539ede8f011
Sabyrka <sabyrkazan@gmail.com>
2026-03-16 19:53:16 +0500

    style: implement RecipeBanner component styles with background, layout, and typography adjustments

--- src/components/RecipeBanner/RecipeBanner.jsx
+++ src/components/RecipeBanner/RecipeBanner.jsx
@@ -1,5 +1,15 @@
 import './RecipeBanner.scss'
+import { Image } from 'minista'
+import dishImage from '@/assets/images/recipe-banner/dish.png'
 
 export default () => {
-  return <div>RecipeBanner</div>
+  return (
+    <article className="recipe-banner">
+      <h3 className="recipe-banner__title">Don’t forget to eat healthy food</h3>
+      <Image className='recipe-banner__image' src={dishImage}/>
+      <a className="recipe-banner__link" href="/">
+        www.foodieland.com
+      </a>
+    </article>
+  )
 }
