5a45de3fda03ada4581a5880598e0a311b5e8514
Sabyrka <sabyrkazan@gmail.com>
2026-03-16 17:04:37 +0500

    feat: enhance RecipeCard with dynamic props, favorite icon, and tags integration

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,5 +1,25 @@
 import './RecipeCard.scss'
+import { Icon } from '@/components/Icon'
+import { Image } from 'minista'
 
-export default () => {
-  return <div>RecipeCard</div>
+export default ({ imgSrc, favoriteImgSrc, title, tags }) => {
+  return (
+    <article className="recipe-card">
+      <div className="recipe-card__image-wrapper">
+        <div className="recipe-card__favorite">
+          <Image src={favoriteImgSrc} />
+        </div>
+        <Image className="recipe-card__image" src={imgSrc} />
+      </div>
+      <h3 className="recipe-card__title">{title}</h3>
+      <ul className="recipe-card__tags">
+        {tags.map(({ icon, label }) => (
+          <li className="recipe-card__tag" key={label}>
+            <Icon name={icon} hasFill />
+            <span>{label}</span>
+          </li>
+        ))}
+      </ul>
+    </article>
+  )
 }
