cca11e3f9819cd9f23b33caf441b36b371b8b448
Sabyrka <sabyrkazan@gmail.com>
2026-03-17 23:09:19 +0500

    feat: style SubscribeForm component with responsive layout adjustments

--- src/sections/SubscribeForm/SubscribeForm.jsx
+++ src/sections/SubscribeForm/SubscribeForm.jsx
@@ -1,5 +1,34 @@
 import './SubscribeForm.scss'
+import { Button } from '@/components/Button'
 
 export default () => {
-  return <div>SubscribeForm</div>
+  return (
+    <section className="subscribe container">
+      <div className="subscribe__inner">
+        <h2 className="subscribe__title">Deliciousness to your inbox</h2>
+        <div className="subscribe__description">
+          <p>
+            Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqut enim ad
+            minim
+          </p>
+        </div>
+        <form className="subscribe__form">
+          <label className="visually-hidden" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="subscribe__input"
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Your email address..."
+          />
+          <Button className="subscribe__button" type="submit">
+            Subscribe
+          </Button>
+        </form>
+      </div>
+    </section>
+  )
 }
