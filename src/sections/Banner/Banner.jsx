29a8305de729d4f7c1703c61652a460acaeaed73
Sabyrka <sabyrkazan@gmail.com>
2026-03-17 17:48:00 +0500

    feat: add banner background image asset for Banner component

--- src/sections/Banner/Banner.jsx
+++ src/sections/Banner/Banner.jsx
@@ -1,5 +1,27 @@
 import './Banner.scss'
+import { Button } from '@/components/Button'
+import { Image } from 'minista'
+import bannerBackgroundImage from '@/assets/images/banner/banner-bg.png'
 
 export default () => {
-  return <div>Banner</div>
+  return (
+    <div className="banner">
+      <div className="banner__inner container">
+        <div className="banner__body">
+          <h2 className="banner__title">
+            Everyone can be a chef in their own kitchen
+          </h2>
+          <div className="banner__description">
+            <p>
+              Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad
+              minim
+            </p>
+          </div>
+          <Button className="banner__button">Learn More</Button>
+        </div>
+        <Image src={bannerBackgroundImage} />
+      </div>
+    </div>
+  )
 }
