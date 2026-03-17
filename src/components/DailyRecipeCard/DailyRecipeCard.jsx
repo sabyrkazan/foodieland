cc8e2a8e49862a7835fa13d346fef62cfc2050f8
Sabyrka <sabyrkazan@gmail.com>
2026-03-17 22:26:25 +0500

    feat: enhance DailyRecipeCard with dynamic props and additional components

--- src/components/DailyRecipeCard/DailyRecipeCard.jsx
+++ src/components/DailyRecipeCard/DailyRecipeCard.jsx
@@ -1,5 +1,18 @@
 import './DailyRecipeCard.scss'
+import { Image } from 'minista'
+import { Tags } from '@/components/Tags'
 
-export default () => {
-  return <div>DailyRecipeCard</div>
+export default ({ imgSrc, favoriteImgSrc, title, tags }) => {
+  return (
+    <article className="daily-recipe-card">
+      <div className="daily-recipe-card__image-wrapper">
+        <div className="daily-recipe-card__favorite">
+          <Image src={favoriteImgSrc} />
+        </div>
+        <Image className="daily-recipe-card__image" src={imgSrc} />
+      </div>
+      <h3 className="daily-recipe-card__title h4">{title}</h3>
+      <Tags className="daily-recipe-card__tags" items={tags} />
+    </article>
+  )
 }
