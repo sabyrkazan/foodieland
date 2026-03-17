5794299b71c3f8108a3574531d797b58b7501b86
Sabyrka <sabyrkazan@gmail.com>
2026-03-17 22:06:39 +0500

    feat: enhance DailyRecipes component with structure and responsive styles

--- src/sections/DailyRecipes/DailyRecipes.jsx
+++ src/sections/DailyRecipes/DailyRecipes.jsx
@@ -1,5 +1,21 @@
 import './DailyRecipes.scss'
+import { Section } from '@/layouts/Section'
+import { Grid } from '@/components/Grid'
+import { DailyRecipeCard } from '@/components/DailyRecipeCard'
 
 export default () => {
-  return <div>DailyRecipes</div>
+  return (
+    <Section
+      title="Try this delicious recipe to make your day"
+      titleClassName='daily-recipes__title'
+      titleId="daily-recipes"
+      description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim "
+      descriptionClassName='daily-recipes__description'
+      headerMode='justify-between'
+    >
+      <Grid columns={4}>
+        <DailyRecipeCard />
+      </Grid>
+    </Section>
+  )
 }
