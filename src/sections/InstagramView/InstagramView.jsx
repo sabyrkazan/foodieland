f864e22bae904e22539c32210e7cf3443ddd62bd
Sabyrka <sabyrkazan@gmail.com>
2026-03-17 18:26:08 +0500

    feat: enhance InstagramView with Section and Grid components

--- src/sections/InstagramView/InstagramView.jsx
+++ src/sections/InstagramView/InstagramView.jsx
@@ -1,5 +1,19 @@
 import './InstagramView.scss'
+import { Section } from '@/layouts/Section'
+import { Grid } from '@/components/Grid'
 
 export default () => {
-  return <div>InstagramView</div>
+  return (
+    <div className="instagram-view">
+      <Section
+        title="Check out @foodieland on Instagram"
+        titleId="instagram-view"
+        description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim"
+        descriptionClassName="instagram-view__description"
+        headerMode="vertical"
+      >
+        <Grid className="recipes__grid" columns={4}></Grid>
+      </Section>
+    </div>
+  )
 }
