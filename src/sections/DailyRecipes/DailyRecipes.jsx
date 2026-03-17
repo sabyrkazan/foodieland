1d77c83ed6f5bcb5bb810ceda19cf2eedab45964
Sabyrka <sabyrkazan@gmail.com>
2026-03-17 22:22:02 +0500

    refactor: map dailyRecipes data into DailyRecipeCard for dynamic rendering

--- src/sections/DailyRecipes/DailyRecipes.jsx
+++ src/sections/DailyRecipes/DailyRecipes.jsx
@@ -2,19 +2,22 @@ import './DailyRecipes.scss'
 import { Section } from '@/layouts/Section'
 import { Grid } from '@/components/Grid'
 import { DailyRecipeCard } from '@/components/DailyRecipeCard'
+import { dailyRecipes } from './daily-recipes.data'
 
 export default () => {
   return (
     <Section
       title="Try this delicious recipe to make your day"
-      titleClassName='daily-recipes__title'
+      titleClassName="daily-recipes__title"
       titleId="daily-recipes"
       description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim "
-      descriptionClassName='daily-recipes__description'
-      headerMode='justify-between'
+      descriptionClassName="daily-recipes__description"
+      headerMode="justify-between"
     >
       <Grid columns={4}>
-        <DailyRecipeCard />
+        {dailyRecipes.map(({ id, recipe }) => (
+          <DailyRecipeCard key={id} {...recipe} />
+        ))}
       </Grid>
     </Section>
   )
