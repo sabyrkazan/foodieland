e158b50d9d71c85a3af8ea34d2f9b0a2a94be4a2
Sabyrka <sabyrkazan@gmail.com>
2026-03-17 22:21:54 +0500

    feat: add daily-recipes.data.js with structured recipe data

---
+++ src/sections/DailyRecipes/daily-recipes.data.js
@@ -0,0 +1,133 @@
+import favoriteIcon from '@/assets/icons/heart/heart-fill.svg'
+import unfavoriteIcon from '@/assets/icons/heart/heart-solid.svg'
+
+export const dailyRecipes = [
+  {
+    id: 1,
+    imgSrc: '',
+    favoriteImgSrc: favoriteIcon,
+    title: 'Mixed Tropical Fruit Salad with Superfood Boosts',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Healthy',
+      },
+    ],
+  },
+  {
+    id: 2,
+    imgSrc: '',
+    favoriteImgSrc: unfavoriteIcon,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Western',
+      },
+    ],
+  },
+  {
+    id: 3,
+    imgSrc: '',
+    favoriteImgSrc: favoriteIcon,
+    title: 'Healthy Japanese Fried Rice with Asparagus',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Healthy',
+      },
+    ],
+  },
+  {
+    id: 4,
+    imgSrc: '',
+    favoriteImgSrc: unfavoriteIcon,
+    title: 'Cauliflower Walnut Vegetarian Taco Meat',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Eastern',
+      },
+    ],
+  },
+  {
+    id: 5,
+    imgSrc: '',
+    favoriteImgSrc: favoriteIcon,
+    title: 'Rainbow Chicken Salad with Almond Honey Mustard Dressing',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Healthy',
+      },
+    ],
+  },
+  {
+    id: 6,
+    imgSrc: '',
+    favoriteImgSrc: unfavoriteIcon,
+    title: 'Barbeque Spicy Sandwiches with Chips',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Snack',
+      },
+    ],
+  },
+  {
+    id: 7,
+    imgSrc: '',
+    favoriteImgSrc: unfavoriteIcon,
+    title: 'Firecracker Vegan Lettuce Wraps - Spicy!',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Seafood',
+      },
+    ],
+  },
+  {
+    id: 8,
+    imgSrc: '',
+    favoriteImgSrc: favoriteIcon,
+    title: 'Chicken Ramen Soup with Mushroom',
+    tags: [
+      {
+        icon: 'clock',
+        label: '30 Minutes',
+      },
+      {
+        icon: 'cutlery',
+        label: 'Japanese',
+      },
+    ],
+  },
+]
